ab02d71b7c981fc95f9155d821fca34dd239de2a
Ghada-Mohamed-Khalaf <gmo625112@gmail.com>
2024-09-16 23:57:11 +0300

    update  number page

--- lib/views/number_page.dart
+++ lib/views/number_page.dart
@@ -15,14 +15,29 @@ class NumberPage extends StatelessWidget {
         color: Colors.orangeAccent,
         child: Row(
           children: [
-            Image.asset("asstes/images/numbers/number_one.png"),
-            Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text("ichi",style: TextStyle(color: Colors.white,fontSize: 18,),),
-                Text("one",style: TextStyle(color: Colors.white,fontSize: 18),),
-              ],
-            )
+            Container(
+              color: Color(0xffE7CECEFF),
+
+
+                child: Image.asset("asstes/images/numbers/number_one.png")),
+            Padding(
+              padding: const EdgeInsets.only(left: 16),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text("ichi",style: TextStyle(color: Colors.white,fontSize: 18,),),
+                  Text("one",style: TextStyle(color: Colors.white,fontSize: 18),),
+
+                ],
+              ),
+            ),
+            Spacer(flex: 1,),
+
+
+            Padding(
+              padding: const EdgeInsets.only(right:  16),
+              child: Icon(Icons.play_arrow,color: Colors.white,size: 33,),
+            ),
           ],
 
         ),
