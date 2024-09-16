0ba1c4cbd1a46924962317d94dc35e3e6bb49652
Ghada-Mohamed-Khalaf <gmo625112@gmail.com>
2024-09-16 23:11:32 +0300

    create number page

--- lib/views/number_page.dart
+++ lib/views/number_page.dart
@@ -5,6 +5,30 @@ class NumberPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Numbers",style: TextStyle(color: Colors.white),),
+        backgroundColor: Colors.brown,
+      ),
+      body: Container(
+        height: 90,
+        color: Colors.orangeAccent,
+        child: Row(
+          children: [
+            Image.asset("asstes/images/numbers/number_one.png"),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text("ichi",style: TextStyle(color: Colors.white,fontSize: 18,),),
+                Text("one",style: TextStyle(color: Colors.white,fontSize: 18),),
+              ],
+            )
+          ],
+
+        ),
+
+      ),
+
+    );
   }
 }
