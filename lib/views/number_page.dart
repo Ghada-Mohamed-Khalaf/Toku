0d390574f68cf9c238f3e06a20180e6753dc1d36
Ghada-Mohamed-Khalaf <gmo625112@gmail.com>
2024-09-17 00:40:16 +0300

    create model

--- lib/views/number_page.dart
+++ lib/views/number_page.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
 
 class NumberPage extends StatelessWidget {
-  const NumberPage({super.key});
+  NumberPage({super.key});
+ final Number one=Number(
+    image: "asstes/images/numbers/number_one.png",
+    jpName: "ichi",
+    enName: "one",
+  );
 
   @override
   Widget build(BuildContext context) {
@@ -19,14 +24,14 @@ class NumberPage extends StatelessWidget {
               color: Color(0xffE7CECEFF),
 
 
-                child: Image.asset("asstes/images/numbers/number_one.png")),
+                child: Image.asset(one.image)),
             Padding(
               padding: const EdgeInsets.only(left: 16),
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  Text("ichi",style: TextStyle(color: Colors.white,fontSize: 18,),),
-                  Text("one",style: TextStyle(color: Colors.white,fontSize: 18),),
+                  Text(one.jpName,style: TextStyle(color: Colors.white,fontSize: 18,),),
+                  Text(one.enName,style: TextStyle(color: Colors.white,fontSize: 18),),
 
                 ],
               ),
@@ -46,4 +51,10 @@ class NumberPage extends StatelessWidget {
 
     );
   }
+}
+class Number{
+ final String image;
+final  String jpName;
+ final String enName;
+const  Number({ required this.image,required this.enName,required this.jpName});
 }
